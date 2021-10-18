ab565e620f73a78d85abe8ca6620198d4f9c1777
John Sutor <johnsutor3@gmail.com>
2021-10-17 22:09:01 -0400

    Fix poor image rendering on articles

--- htmlserializer.js
+++ htmlserializer.js
@@ -2,16 +2,8 @@ var PrismicDOM = require('prismic-dom');
 import Image from 'next/image'
 var Elements = PrismicDOM.RichText.Elements;
 
-const imageLoader = ({ src, width, height }) => {
-    return `${src}?fit=crop&crop=faces&w=${width || 256}&h=${height || 256}`
-}
-
 export default function htmlSerializer(type, element, content, children) {
     switch (type) {
-
-        // Don't wrap images in a <p> tag
-        case Elements.image:
-            return (<Image src={element.url} width={582} height={389} loader={imageLoader} className="w-full" />)
 
         case Elements.embed:
             return (
