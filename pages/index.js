1e84a7d76ebae5451eaeea11096303b8abe762d5
Luke Sutor <lukesutor@gmail.com>
2021-12-26 18:26:41 -0500

    add all animations to index

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ export default function Home() {
   const [animateLanding, setAnimateLanding] = useState(false)
   const [animatePartners, setAnimatePartners] = useState(false)
   const [animateMission, setAnimateMission] = useState(false)
+  const [animateTestimonials, setAnimateTestimonials] = useState(false)
+  const [animateMedia, setAnimateMedia] = useState(false)
 
 
   function renderDesk(canvas) {
@@ -29,6 +31,8 @@ export default function Home() {
     // Intersection Observer Stuff
     const partners = document.getElementById('partners')
     const mission = document.getElementById('mission')
+    const testimonials = document.getElementById('testimonials')
+    const media = document.getElementById('media')
 
     const observer = new IntersectionObserver(
       entries => {
@@ -44,6 +48,17 @@ export default function Home() {
                 setAnimateMission(true)
                 observer.unobserve(mission)
                 break;
+
+              case 'testimonials':
+                setAnimateTestimonials(true)
+                observer.unobserve(testimonials)
+                break;
+
+              case 'media':
+                setAnimateMedia(true)
+                observer.unobserve(media)
+                break;
+
               default:
                 break;
             }
@@ -58,6 +73,13 @@ export default function Home() {
     // Add all elements to observer
     observer.observe(partners)
     observer.observe(mission)
+    observer.observe(testimonials)
+    observer.observe(media)
+
+    if (window.innerWidth >= 1) {
+      testimonials_arr.push()
+    }
+    console.log(testimonials_arr);
   }, [])
 
   const { t } = useTranslation('common');
@@ -97,9 +119,7 @@ export default function Home() {
       }
     })
 
-  const missionSpring = useSpring({
-    opacity: animateMission ? 1 : 0, transform: animateMission ? 'translateX(0)' : 'translateX(-120px)', delay: 500, config: config.molasses
-  })
+
   const missionTrail = useTrail(2,
     {
       opacity: animateMission ? 1 : 0,
@@ -109,6 +129,96 @@ export default function Home() {
         transform: 'translateY(50px) rotate(3deg)'
       }
     })
+  const missionSpring = useSpring({
+    opacity: animateMission ? 1 : 0, transform: animateMission ? 'translateX(0)' : 'translateX(-120px)', delay: 400, config: config.molasses
+  })
+
+
+  const testimonialsTitleSpring = useSpring({
+    opacity: animateTestimonials ? 1 : 0,
+    transform: animateTestimonials ? 'translateY(0px) rotate(0)' : 'translateY(-50px) rotate(-3deg)',
+    from: {
+      opacity: 0,
+      transform: 'translateY(-50px) rotate(-3deg)'
+    }
+  })
+  let testimonials_arr = [
+    {
+      'body': "Working with students across the world with SciTeens was amazing... I am super grateful that I was able to experience it.",
+      'name': 'David L.',
+      'country': 'United States',
+      'image': './assets/zondicons/globe.svg'
+    },
+    {
+      'body': "I want to first thank the members and founders of SciTeens for bringing such a wonderful and amazing opportunity we are having.I would love to thank all the mentors who were taking most of their time mentoring us as well helping us coming up with best projects.",
+      'name': 'Elisha M.',
+      'country': 'Zimbabwe',
+      'image': './assets/zondicons/education.svg'
+    },
+    {
+      'body': "Because of the coding boot camp I did with SciTeens, I was able to find my passion for coding and further expand my knowledge in the STEM field.",
+      'name': 'Melissa R.',
+      'country': 'United States',
+      'image': './assets/zondicons/code.svg'
+    }
+  ]
+  const testimonialsTrail = useTrail(testimonials_arr.length,
+    {
+      opacity: animateTestimonials ? 1 : 0,
+      transform: animateTestimonials ? 'scale(1)' : 'scale(0)',
+      from: {
+        opacity: 0,
+        transform: 'scale(0)'
+      },
+      delay: 400,
+      config: config.gentle
+    })
+  const testimonialsStatsSpring = useSpring({
+    opacity: animateTestimonials ? 1 : 0,
+    transform: animateTestimonials ? 'translateY(0px)' : 'translateY(50px)',
+    from: {
+      opacity: 0,
+      transform: 'translateY(50px)'
+    },
+    delay: 900,
+    config: config.molasses
+  })
+
+  const mediaTitleSpring = useSpring({
+    opacity: animateMedia ? 1 : 0,
+    transform: animateMedia ? 'translateY(0px) rotate(0)' : 'translateY(50px) rotate(3deg)',
+    from: {
+      opacity: 0,
+      transform: 'translateY(50px) rotate(3deg)'
+    }
+  })
+  const mediaSpring1 = useSpring({
+    opacity: animateMedia ? 1 : 0,
+    transform: animateMedia ? 'translateX(0px)' : 'translateX(-100px)',
+    from: {
+      opacity: 0,
+      transform: 'translateX(-100px)'
+    },
+    delay: 300
+  })
+  const mediaSpring2 = useSpring({
+    opacity: animateMedia ? 1 : 0,
+    transform: animateMedia ? 'translateX(0px)' : 'translateX(100px)',
+    from: {
+      opacity: 0,
+      transform: 'translateX(100px)'
+    },
+    delay: 500
+  })
+  const mediaSpring3 = useSpring({
+    opacity: animateMedia ? 1 : 0,
+    transform: animateMedia ? 'translateX(0px)' : 'translateX(100px)',
+    from: {
+      opacity: 0,
+      transform: 'translateX(100px)'
+    },
+    delay: 700
+  })
 
   return (
     <div>
@@ -164,10 +274,10 @@ export default function Home() {
         {/* Mission Statement & Information */}
         <div className="mb-28 md:mb-36">
           <div className="flex flex-col lg:flex-row mx-5 md:mx-16 lg:mx-24">
-            <div className='relative w-3/5 transition-all'>
+            <div className='relative w-11/12 lg:w-3/5 mx-auto transition-all'>
               <animated.img style={missionSpring} id="mission_img" src='assets/device_mockup.png' alt="Computer and phone showing sciteens website" />
             </div>
-            <div id="mission" className='w-2/5 my-auto'>
+            <div id="mission" className='w-5/6 lg:w-2/5 mt-6 lg:my-auto mx-auto'>
               {missionTrail.map((styles, index) => {
                 return index == 0 ?
                   <animated.h2 style={styles} className="text-xl md:text-3xl lg:text-4xl font-semibold mb-4 ml-12">
@@ -185,35 +295,21 @@ export default function Home() {
 
         {/* Open Source & Testimonials */}
         <div className="mb-28 md:mb-36">
-          <h2 className="text-center text-xl md:text-3xl lg:text-5xl font-semibold mb-12 mx-12 md:mx-28 lg:mx-48">
+          <animated.h2 style={testimonialsTitleSpring} className="text-center text-xl md:text-3xl lg:text-5xl font-semibold mb-12 mx-12 md:mx-28 lg:mx-48">
             {t('index.sciteens_pride')}
-          </h2>
-          <div className="relative flex flex-col md:flex-row items-center md:items-stretch justify-between mx-5 md:mx-16 lg:mx-24 z-10">
-            <div className="bg-white shadow p-5 rounded-lg w-auto md:w-[45%] lg:w-[30%] mb-8">
-              <img src={'./assets/zondicons/globe.svg'} className="h-10 mb-6" alt="" />
-              <p className="text-sm lg:text-base mb-4">
-                "Working with students across the world with SciTeens was amazing... I am super grateful that I was
-                able to experience it."
-              </p>
-              <p className="text-lg lg:text-xl">- <span className="font-semibold">David L.</span>, United States</p>
-            </div>
-            <div className="bg-white shadow p-5 rounded-lg w-auto md:w-[45%] lg:w-[30%] mb-8 md:mb-0">
-              <img src={'./assets/zondicons/education.svg'} className="h-10 mb-6" alt="" />
-              <p className="text-sm lg:text-base mb-4">
-                "I want to first thank the members and founders of SciTeens for bringing such a wonderful and amazing opportunity
-                we are having.I would love to thank all the mentors who were taking most of their time mentoring us as well
-                helping us coming up with best projects."
-              </p>
-              <p className="text-lg lg:text-xl">- <span className="font-semibold">Elisha M.</span>, Zimbabwe</p>
-            </div>
-            <div className="hidden lg:flex flex-col items-baseline bg-white shadow p-5 rounded-lg w-[30%] mb-8">
-              <img src={'./assets/zondicons/code.svg'} className="h-10 mb-6" alt="" />
-              <p className="text-sm lg:text-base mb-4">
-                "Because of the coding boot camp I did with SciTeens, I was able to find my passion for coding and
-                further expand my knowledge in the STEM field."
-              </p>
-              <p className="text-lg lg:text-xl">- <span className="font-semibold">Melissa R.</span>, United States</p>
-            </div>
+          </animated.h2>
+          <div id='testimonials' className="relative flex flex-col md:flex-row items-center md:items-stretch justify-between mx-5 md:mx-16 lg:mx-24 z-10">
+            {testimonialsTrail.map((styles, index) => {
+              return (
+                <animated.div style={styles} className="last-of-type:hidden lg:last-of-type:block bg-white shadow p-5 rounded-lg w-auto md:w-[45%] lg:w-[30%] lg:first-of-type:mb-8 lg:last-of-type:mb-8">
+                  <animated.img src={testimonials_arr[index].image} className="h-10 mb-6" alt="" />
+                  <animated.p className="text-sm lg:text-base mb-4">
+                    {testimonials_arr[index].body}
+                  </animated.p>
+                  <animated.p className="text-lg lg:text-xl">- <span className="font-semibold">{testimonials_arr[index].name}</span>, {testimonials_arr[index].country}</animated.p>
+                </animated.div>
+              )
+            })}
           </div>
           <svg viewBox="0 0 900 40" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1" className="-mt-36">
             <rect x="0" y="0" width="100%" height="100%" fill="#F5FFF5" />
@@ -221,25 +317,27 @@ export default function Home() {
             <path d="M0 18L10.7 17.8C21.3 17.7 42.7 17.3 64.2 17.3C85.7 17.3 107.3 17.7 128.8 18.7C150.3 19.7 171.7 21.3 193 22.5C214.3 23.7 235.7 24.3 257 24C278.3 23.7 299.7 22.3 321.2 21C342.7 19.7 364.3 18.3 385.8 19.2C407.3 20 428.7 23 450 24.5C471.3 26 492.7 26 514.2 24.3C535.7 22.7 557.3 19.3 578.8 19.5C600.3 19.7 621.7 23.3 643 24.3C664.3 25.3 685.7 23.7 707 22.2C728.3 20.7 749.7 19.3 771.2 18.5C792.7 17.7 814.3 17.3 835.8 17.2C857.3 17 878.7 17 889.3 17L900 17L900 41L889.3 41C878.7 41 857.3 41 835.8 41C814.3 41 792.7 41 771.2 41C749.7 41 728.3 41 707 41C685.7 41 664.3 41 643 41C621.7 41 600.3 41 578.8 41C557.3 41 535.7 41 514.2 41C492.7 41 471.3 41 450 41C428.7 41 407.3 41 385.8 41C364.3 41 342.7 41 321.2 41C299.7 41 278.3 41 257 41C235.7 41 214.3 41 193 41C171.7 41 150.3 41 128.8 41C107.3 41 85.7 41 64.2 41C42.7 41 21.3 41 10.7 41L0 41Z" fill="#439e70" />
             <path d="M0 32L10.7 31C21.3 30 42.7 28 64.2 28C85.7 28 107.3 30 128.8 29.8C150.3 29.7 171.7 27.3 193 26.7C214.3 26 235.7 27 257 27.3C278.3 27.7 299.7 27.3 321.2 28.3C342.7 29.3 364.3 31.7 385.8 32.2C407.3 32.7 428.7 31.3 450 30C471.3 28.7 492.7 27.3 514.2 27.7C535.7 28 557.3 30 578.8 31.3C600.3 32.7 621.7 33.3 643 33.2C664.3 33 685.7 32 707 31.2C728.3 30.3 749.7 29.7 771.2 29.3C792.7 29 814.3 29 835.8 28.8C857.3 28.7 878.7 28.3 889.3 28.2L900 28L900 41L889.3 41C878.7 41 857.3 41 835.8 41C814.3 41 792.7 41 771.2 41C749.7 41 728.3 41 707 41C685.7 41 664.3 41 643 41C621.7 41 600.3 41 578.8 41C557.3 41 535.7 41 514.2 41C492.7 41 471.3 41 450 41C428.7 41 407.3 41 385.8 41C364.3 41 342.7 41 321.2 41C299.7 41 278.3 41 257 41C235.7 41 214.3 41 193 41C171.7 41 150.3 41 128.8 41C107.3 41 85.7 41 64.2 41C42.7 41 21.3 41 10.7 41L0 41Z" fill="#2d8a5b" />
           </svg>
-          <div className="flex flex-row justify-evenly md:justify-between px-0 md:px-24 pt-32 pb-10 text-white text-center bg-sciteensGreen-regular">
-            <div className="w-[30%] md:w-[45%] lg:w-[30%] mr-10">
-              <p className="text-3xl md:text-4xl lg:text-5xl font-semibold">400+</p>
-              <p className="text-sm md:text-base text-gray-300">
-                {t('index.monthly_users')}
-              </p>
-            </div>
-            <div className="w-[30%] md:w-[45%] lg:w-[30%]">
-              <p className="text-3xl md:text-4xl lg:text-5xl font-semibold">50</p>
-              <p className="text-sm md:text-base text-gray-300">
-                {t('index.schools')}
-              </p>
-            </div>
-            <div className="hidden lg:flex flex-col w-[45%] lg:w-[30%]">
-              <p className="text-4xl lg:text-5xl font-semibold">7</p>
-              <p className="text-base text-gray-300">
-                {t('index.countries')}
-              </p>
-            </div>
+          <div className="px-0 md:px-24 pt-32 pb-10 text-white text-center bg-sciteensGreen-regular">
+            <animated.div style={testimonialsStatsSpring} className='flex flex-row justify-evenly md:justify-between '>
+              <div className="w-[30%] md:w-[45%] lg:w-[30%] mr-10">
+                <p className="text-3xl md:text-4xl lg:text-5xl font-semibold">400+</p>
+                <p className="text-sm md:text-base text-gray-300">
+                  {t('index.monthly_users')}
+                </p>
+              </div>
+              <div className="w-[30%] md:w-[45%] lg:w-[30%]">
+                <p className="text-3xl md:text-4xl lg:text-5xl font-semibold">50</p>
+                <p className="text-sm md:text-base text-gray-300">
+                  {t('index.schools')}
+                </p>
+              </div>
+              <div className="hidden lg:flex flex-col w-[45%] lg:w-[30%]">
+                <p className="text-4xl lg:text-5xl font-semibold">7</p>
+                <p className="text-base text-gray-300">
+                  {t('index.countries')}
+                </p>
+              </div>
+            </animated.div>
           </div>
           <svg viewBox="0 0 900 40" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
             <rect x="0" y="0" width="100%" height="100%" fill="#F5FFF5" />
@@ -252,11 +350,11 @@ export default function Home() {
 
         {/* Featured Media */}
         <div className="mb-28 md:mb-36">
-          <h2 className="text-center text-xl md:text-3xl lg:text-5xl font-semibold mb-12">
+          <animated.h2 style={mediaTitleSpring} className="text-center text-xl md:text-3xl lg:text-5xl font-semibold mb-12">
             {t('index.featured_media')}
-          </h2>
-          <div className="grid grid-rows-3 grid-cols-1 lg:grid-rows-2 lg:grid-cols-2 mx-5 md:mx-16 lg:mx-24">
-            <a href="https://www.neonscience.org/impact/observatory-blog/sciteens-data-science-and-ecology-gen-z"
+          </animated.h2>
+          <div id='media' className="grid grid-rows-3 grid-cols-1 lg:grid-rows-2 lg:grid-cols-2 mx-5 md:mx-16 lg:mx-24">
+            <animated.a style={mediaSpring1} href="https://www.neonscience.org/impact/observatory-blog/sciteens-data-science-and-ecology-gen-z"
               className="group col-span-1 row-span-1 lg:row-span-2 bg-white rounded-lg shadow overflow-hidden mr-0 lg:mr-4 mb-4 lg:mb-0"
               target="_blank" rel="noopener noreferrer">
               <div className="h-full flex flex-col md:flex-row lg:flex-col">
@@ -270,8 +368,8 @@ export default function Home() {
                   <p className="text-sm md:text-lg text-gray-700">February 10, 2021</p>
                 </div>
               </div>
-            </a>
-            <a href="https://news.mit.edu/2019/ideas-challenge-social-ventures-0430"
+            </animated.a>
+            <animated.a style={mediaSpring2} href="https://news.mit.edu/2019/ideas-challenge-social-ventures-0430"
               className="group  row-span-1 col-span-1 bg-white rounded-lg shadow overflow-hidden ml-0 lg:ml-4 mb-4 mt-4 lg:mt-0"
               target="_blank" rel="noopener noreferrer">
               <div className="h-full flex flex-col md:flex-row">
@@ -283,8 +381,8 @@ export default function Home() {
                   <p className="text-sm md:text-lg text-gray-700">April 30, 2019</p>
                 </div>
               </div>
-            </a>
-            <a href="https://news.fsu.edu/multimedia/radio/2020/08/03/young-scholars-online-program-turns-students-into-scientists/"
+            </animated.a>
+            <animated.a style={mediaSpring3} href="https://news.fsu.edu/multimedia/radio/2020/08/03/young-scholars-online-program-turns-students-into-scientists/"
               className="group row-span-1 col-span-1 bg-white rounded-lg shadow overflow-hidden h-72 md:h-auto ml-0 lg:ml-4 mt-4"
               target="_blank" rel="noopener noreferrer">
               <div className="h-full flex flex-col md:flex-row lg:flex-row-reverse">
@@ -298,7 +396,7 @@ export default function Home() {
                   <p className="text-sm md:text-lg text-gray-700 pb-5">August 3, 2020</p>
                 </div>
               </div>
-            </a>
+            </animated.a>
           </div>
         </div>
       </div>
